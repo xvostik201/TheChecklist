19a4703a39bd087e907dd5127be028572e2057a9
Zakhar <zkostyutkin2004@gmail.com>
2026-04-18 19:28:52 +0500

    ref: optimize inspector drawing logic in CockpitElementEditor

--- Assets/_Projects/Scripts/Editor/CockpitElementEditor.cs
+++ Assets/_Projects/Scripts/Editor/CockpitElementEditor.cs
@@ -13,31 +13,40 @@ public class CockpitElementEditor : Editor
         serializedObject.Update();
         
         DrawPropertiesExcluding(serializedObject,
-            "_targetRotation", "_targetPosition", 
-            "_minRotation", "_maxRotation", "_dragSpeed");
+            "_targetRotation", "_targetPositionZ", 
+            "_minRotation", "_maxRotation", "_dragSpeed", "_animationDuration");
+        
 
         CockpitElementData data = (CockpitElementData)target;
         
         EditorGUILayout.Space();
+        
         EditorGUILayout.LabelField("Specific settings", EditorStyles.boldLabel);
         
         switch (data.ElementType)
         {
             case CockpitElementType.Button:
-                EditorGUILayout.PropertyField(serializedObject.FindProperty("_targetPositionZ"), 
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("_targetPositionZ"),
+                    true);
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("_animationDuration"), 
                     true);
                 EditorGUILayout.HelpBox($"Current element type: BUTTON", MessageType.Info);
                 break;
             case CockpitElementType.Lever:
                 EditorGUILayout.PropertyField(serializedObject.FindProperty("_targetRotation"), 
                     true);
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("_animationDuration"),
+                    true); 
                 EditorGUILayout.HelpBox($"Current element type: LEVER", MessageType.Info);
                 
                 break;
             case CockpitElementType.Dragging:
-                EditorGUILayout.PropertyField(serializedObject.FindProperty("_minRotation"), true);
-                EditorGUILayout.PropertyField(serializedObject.FindProperty("_maxRotation"), true);
-                EditorGUILayout.PropertyField(serializedObject.FindProperty("_dragSpeed"), true);
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("_minRotation"),
+                    true);
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("_maxRotation"),
+                    true);
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("_dragSpeed"),
+                    true);
                     
                 EditorGUILayout.HelpBox($"Current element type: DRAGGING", MessageType.Info);
                 break;
